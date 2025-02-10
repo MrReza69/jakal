cfe879c44c8522ed041ec57b5fd03ab1590f6023
MrReza69 <rezaeizia09@gmail.com>
2025-02-10 20:26:01 +0330

    Update models.py

--- models.py
+++ models.py
@@ -13,3 +13,13 @@ class Search(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     query = db.Column(db.String(200), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+class SavedTrack(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    track_id = db.Column(db.String(200), nullable=False)  # ID آهنگ از اسپاتیفای
+    track_name = db.Column(db.String(200), nullable=False)
+    artist_name = db.Column(db.String(200), nullable=False)
+    added_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"SavedTrack('{self.track_name}', '{self.artist_name}')"
